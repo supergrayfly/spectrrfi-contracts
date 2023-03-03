d23c06d3adf90b6d5564126855e416ca7b621baa
Supergrayfly <supergrayfly@proton.me>
2023-03-03 08:07:38 +0300

    Minor changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,23 @@
 const hre = require("hardhat");
 
 async function main() {
+// deploy payment splitter contract
+  const SpectrrPaymentSplitter = await hre.ethers.getContractFactory("SpectrrPaymentSplitter");
+  const spectrrPaymentSplitter = await SpectrrPaymentSplitter.deploy();
+
+  await spectrrPaymentSplitter.deployed();
+
+	let splitterAddress = spectrrPaymentSplitter.address
+
+  console.log(`Payment Splitter Contract Address: "${splitterAddress}"`);
+
+// deploy spctrrfi contract
   const SpectrrFi = await hre.ethers.getContractFactory("SpectrrFi");
-  const spectrrFi = await SpectrrFi.deploy();
+  const spectrrFi = await SpectrrFi.deploy(splitterAddress);
 
   await spectrrFi.deployed();
 
-  console.log(`Contract Address: "${spectrrFi.address}"`);
+  console.log(`SpectrrFi Contract Address: "${spectrrFi.address}"`);
 }
 
 main().catch((error) => {
